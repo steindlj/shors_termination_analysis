fbe0b628ded99386aab09eaa7a101c289ed5e6fd
J. Steindl <70030873+steindlj@users.noreply.github.com>
2025-09-28 00:11:23 +0200

    Update __init__.py

--- shors_termination_analysis/__init__.py
+++ shors_termination_analysis/__init__.py
@@ -4,7 +4,7 @@ shors_termination_analysis
 Empirical analysis of termination criteria in Shor's algorithm.
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 from .core import shors_algorithm, order_finding
 from .analysis import Analysis
